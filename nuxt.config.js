2c77589ab578a52ccaae9c678f8371c536a47738
GREEB <gr44bli@gmail.com>
2021-12-12 17:48:20 +0100

    nuxt: favicon and title changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - ForzaPointCloud',
-    title: ' 🌍 ',
+    titleTemplate: '%s ForzaPointCloud',
+    title: '',
     htmlAttrs: {
       lang: 'en'
     },
